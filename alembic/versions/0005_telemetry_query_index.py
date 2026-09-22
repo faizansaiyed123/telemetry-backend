fec3ae42a95e8eb54a5dc082fe131d9178aa8b65
Faizan Saiyed <fai1ggj@gmail.com>
2026-09-22 14:57:32 +0530

    fix(db): link query index migration to SLO revision

--- alembic/versions/0005_telemetry_query_index.py
+++ alembic/versions/0005_telemetry_query_index.py
@@ -7,7 +7,7 @@ Revises: 0003_ingestion_idempotency
 from alembic import op
 
 revision = "0005_telemetry_query_index"
-down_revision = "0003_slos"
+down_revision = "0004_slos"
 branch_labels = None
 depends_on = None
 
